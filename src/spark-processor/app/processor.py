232e127dd22a173beadf4943498a587e8decb91d
BrenoLD94 <breno.ld@hotmail.com>
2025-09-08 22:36:17 -0300

    Feat: Implementa cold path com MinIO e Dockerfile downloads

--- src/spark-processor/app/processor.py
+++ src/spark-processor/app/processor.py
@@ -7,9 +7,6 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 import os
 
-#  https://spark.apache.org/docs/latest/streaming/getting-started.html#programming-model -> leia isso
-#  https://spark.apache.org/docs/latest/streaming/apis-on-dataframes-and-datasets.html#window-operations-on-event-time 
-
 #{
 #    "stream":"btcusdt@aggTrade",
 #    "data":{"e":"aggTrade","E":1752193668853,"a":2782814634,"s":"BTCUSDT","p":"115239.90","q":"0.070","f":6460726680,"l":6460726680,"T":1752193668763,"m":true}
@@ -30,82 +27,46 @@ import os
 
 KAFKA_TOPIC = "binance-trades-raw"
 KAFKA_BOOSTSTRAP_SERVERS = "kafka:29092"
-
-spark = SparkSession \
-        .builder \
-        .appName("cryptoDataPipelineStreaming") \
-        .master("spark://spark-master:7077") \
-        .config("spark.sql.caseSensitive", "true") \
-        .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
-        .getOrCreate()
-
-user_schema = StructType([ \
-    StructField("e", StringType(), True), \
-    StructField("E", LongType(), True), \
-    StructField("s", StringType(), True), \
-    StructField("a", LongType(), True), \
-    StructField("p", StringType(), True), \
-    StructField("q", StringType(), True), \
-    StructField("f", LongType(), True), \
-    StructField("l", LongType(), True), \
-    StructField("T", LongType(), True), \
-    StructField("m", BooleanType(), True)
-  ])
-
-# defining source
-binance_df_raw = spark \
-                .readStream \
-                .format("kafka") \
-                .option("kafka.bootstrap.servers", KAFKA_BOOSTSTRAP_SERVERS) \
-                .option("subscribe", KAFKA_TOPIC) \
-                .load()
-
-# 1. Cast para String
-df_string = binance_df_raw.selectExpr("CAST(value as STRING) AS value")
-
-# 2. Extrai o JSON de dentro do campo 'data'
-df_data_str = df_string.select(sf.get_json_object(sf.col("value"), "$.data").alias("data_str"))
-
-# 3. Aplica o schema na nova coluna e expande para colunas finais
-df_final_cols = df_data_str.select(sf.from_json(sf.col("data_str"), user_schema).alias("data_struct")) \
-                           .select("data_struct.*")
-
-# 4. Renomeia as colunas
-df_renamed = df_final_cols.withColumnRenamed("e", "event_type") \
-                        .withColumnRenamed("E", "event_time") \
-                        .withColumnRenamed("s", "symbol") \
-                        .withColumnRenamed("a", "agg_trade_id") \
-                        .withColumnRenamed("p" , "price") \
-                        .withColumnRenamed("q", "quantity") \
-                        .withColumnRenamed("f", "first_trade_id") \
-                        .withColumnRenamed("l", "last_trade_id") \
-                        .withColumnRenamed("T", "trade_time") \
-                        .withColumnRenamed("m", "is_buyer_market")
-
-
-df_casted = df_renamed.withColumn("quantity", sf.col("quantity").cast("double")) \
-                    .withColumn("price", sf.col("price").cast("double"))
-
-# 5. Estruturando coluna de event time
-df_with_timestamp = df_casted.withColumn("event_timestamp", (sf.col("event_time") / 1000).cast("timestamp") )
-
-# 6. Aplicando window function e agregações
-df_windowed = df_with_timestamp.withWatermark("event_timestamp", "15 seconds").groupBy(
-    sf.col("symbol"),
-    sf.window(sf.col("event_timestamp"), "10 seconds")
-).agg(
-    sf.sum("quantity").alias("total_quantity"), 
-    sf.avg("price").alias("average_price")
-)
-
-df_windowed.printSchema()
-
 INFLUXDB_BUCKET = "trades_raw"
 INFLUXDB_ORG = "crypto_pipeline_org"
 INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
 INFLUXDB_URL="http://influxdb:8086"
 
+def get_spark_session(catalog_name="cripto-data", 
+                      postgres_user="breno", 
+                      postgres_password="admin2025", 
+                      postgres_db="criptoDB", 
+                      minio_user="breno", 
+                      minio_password="admin2025"):
+
+    spark = SparkSession \
+            .builder \
+            .appName("cryptoDataPipelineStreaming") \
+            .master("spark://spark-master:7077") \
+            .config("spark.sql.caseSensitive", "true") \
+            .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
+            .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+            .config(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog") \
+            .config(f"spark.sql.catalog.{catalog_name}.type", "jdbc") \
+            .config(f"spark.sql.catalog.{catalog_name}.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
+            .config(f"spark.sql.catalog.{catalog_name}.warehouse", f"s3a://{catalog_name}/") \
+            .config(f"spark.sql.catalog.{catalog_name}.uri", f"jdbc:postgresql://postgres:5432/{postgres_db.replace("-", "_")}") \
+            .config(f"spark.sql.catalog.{catalog_name}.jdbc.verifyServerCertificate", "False") \
+            .config(f"spark.sql.catalog.{catalog_name}.jdbc.useSSL", "False") \
+            .config(f"spark.sql.catalog.{catalog_name}.jdbc.user", postgres_user) \
+            .config(f"spark.sql.catalog.{catalog_name}.jdbc.password", postgres_password) \
+            .config("spark.hadoop.fs.s3a.access.key", minio_user) \
+            .config("spark.hadoop.fs.s3a.secret.key", minio_password) \
+            .config("spark.hadoop.fs.s3a.path.style.access", "True") \
+            .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+            .getOrCreate()
+    
+    return spark
+
 def write_influxdb(batch_df, batch_id):
+
+    # =============== HOT PATH =====================
     print(f"--- Processando Lote ID: {batch_id} ---")
     
     client = influxdb_client.InfluxDBClient(
@@ -134,12 +95,109 @@ def write_influxdb(batch_df, batch_id):
     
     client.close()
 
-# 7. Inicia o sink (saída) para o console
-query = df_windowed.writeStream \
-    .foreachBatch(write_influxdb) \
-    .option("checkpointLocation", "/tmp/spark_checkpoints/influxdb_sink") \
-    .trigger(processingTime='15 seconds') \
-    .start()
+def write_raw_to_iceberg(batch_df, batch_id):
+      # =============== COLD PATH =====================
+    try:
+        print(f"--- Escrevendo lote {batch_id} no Iceberg (Cold Path) ---")
+        
+        batch_df.withColumn("trade_date", sf.to_date(sf.col("event_timestamp")))
 
-# 8. Mantém a aplicação viva, esperando o stream terminar
-query.awaitTermination()
+        batch_df.writeTo("cripto-data.bronze.trades_agg") \
+        .partitionedBy("trade_date") \
+        .append()
+        
+        print(f"--- Lote {batch_id} escrito no Iceberg com sucesso. ---")
+    
+    except Exception as e:
+        print(f"Erro ao escrever no Iceberg: {e}")
+
+    # Libera o DataFrame da memória
+    batch_df.unpersist()
+    
+
+def main():
+
+    spark = get_spark_session()
+
+    user_schema = StructType([ \
+        StructField("e", StringType(), True), \
+        StructField("E", LongType(), True), \
+        StructField("s", StringType(), True), \
+        StructField("a", LongType(), True), \
+        StructField("p", StringType(), True), \
+        StructField("q", StringType(), True), \
+        StructField("f", LongType(), True), \
+        StructField("l", LongType(), True), \
+        StructField("T", LongType(), True), \
+        StructField("m", BooleanType(), True)
+      ])
+
+    # defining source
+    binance_df_raw = spark \
+                    .readStream \
+                    .format("kafka") \
+                    .option("kafka.bootstrap.servers", KAFKA_BOOSTSTRAP_SERVERS) \
+                    .option("subscribe", KAFKA_TOPIC) \
+                    .load()
+
+    # 1. Cast para String
+    df_string = binance_df_raw.selectExpr("CAST(value as STRING) AS value")
+
+    # 2. Extrai o JSON de dentro do campo 'data'
+    df_data_str = df_string.select(sf.get_json_object(sf.col("value"), "$.data").alias("data_str"))
+
+    # 3. Aplica o schema na nova coluna e expande para colunas finais
+    df_final_cols = df_data_str.select(sf.from_json(sf.col("data_str"), user_schema).alias("data_struct")) \
+                               .select("data_struct.*")
+
+    # 4. Renomeia as colunas
+    df_renamed = df_final_cols.withColumnRenamed("e", "event_type") \
+                            .withColumnRenamed("E", "event_time") \
+                            .withColumnRenamed("s", "symbol") \
+                            .withColumnRenamed("a", "agg_trade_id") \
+                            .withColumnRenamed("p" , "price") \
+                            .withColumnRenamed("q", "quantity") \
+                            .withColumnRenamed("f", "first_trade_id") \
+                            .withColumnRenamed("l", "last_trade_id") \
+                            .withColumnRenamed("T", "trade_time") \
+                            .withColumnRenamed("m", "is_buyer_market")
+
+
+    df_casted = df_renamed.withColumn("quantity", sf.col("quantity").cast("double")) \
+                        .withColumn("price", sf.col("price").cast("double"))
+
+    # 5. Estruturando coluna de event time
+    df_with_timestamp = df_casted.withColumn("event_timestamp", (sf.col("event_time") / 1000).cast("timestamp") )
+
+    # SINK - COLD PATH
+    query_raw = df_with_timestamp.writeStream \
+        .outputMode("append") \
+        .foreachBatch(write_raw_to_iceberg) \
+        .option("checkpointLocation", "/tmp/spark_checkpoints/cold_path_sink") \
+        .trigger(processingTime='15 seconds') \
+        .start()
+
+
+    # 6. Aplicando window function e agregações
+    df_windowed = df_with_timestamp.withWatermark("event_timestamp", "15 seconds").groupBy(
+        sf.col("symbol"),
+        sf.window(sf.col("event_timestamp"), "10 seconds")
+    ).agg(
+        sf.sum("quantity").alias("total_quantity"), 
+        sf.avg("price").alias("average_price")
+    )
+
+    # SINK - HOT PATH
+    query_aggregated = df_windowed.writeStream \
+        .outputMode("update") \
+        .foreachBatch(write_influxdb) \
+        .option("checkpointLocation", "/tmp/spark_checkpoints/influxdb_sink") \
+        .trigger(processingTime='15 seconds') \
+        .start()
+
+
+    # 8. Mantém a aplicação viva, esperando o stream terminar
+    spark.streams.awaitAnyTermination()
+
+if __name__ == "__main__":
+    main()
